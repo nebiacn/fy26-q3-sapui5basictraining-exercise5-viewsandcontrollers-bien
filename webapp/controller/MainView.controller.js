ac67c36992abe6566412fb829a52687269b8e70c
Bien Gilzon Lara <bien.gilzon.c.lara@accenture.com>
2026-05-21 04:00:31 +0000

    Controller changes: Step 10

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -1,6 +1,12 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller, MessageToast) {
+
     "use strict";
 
     return Controller.extend("com.training.exer5lara.controller.MainView", {
