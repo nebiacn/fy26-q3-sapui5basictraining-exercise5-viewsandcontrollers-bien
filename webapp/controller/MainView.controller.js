6717419fd75dff1217bcb7c35bdfab84266080c6
Bien Gilzon Lara <bien.gilzon.c.lara@accenture.com>
2026-05-21 03:44:13 +0000

    Initial commit

---
+++ webapp/controller/MainView.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("com.training.exer5lara.controller.MainView", {
+        onInit() {
+        }
+    });
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"com/training/exer5lara/test/unit/controller/MainView.controller"
+], function () {
+	"use strict";
+});
