ff3681ea5fffff0cade48c71d707171f27f8fe00
Bien Gilzon Lara <bien.gilzon.c.lara@accenture.com>
2026-05-21 05:05:45 +0000

    add credit card detials input

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -26,22 +26,40 @@ sap.ui.define([
                 var sSelectedKey = oEvent.getParameter("selectedItem").getProperty("key");
                 var oMobileLabel = this.getView().byId("idLblPhone");
                 var oMobileInput = this.getView().byId("idInputPhone");
+                var oCCDetailsLabel = this.getView().byId("idLblCCDetails");
+                var oCCDetailsInput = this.getView().byId("idInputCCDetails");
 
                 if (sSelectedKey === "GCASH"){
                     // show the mobile field
                     oMobileLabel.setVisible(true);
                     oMobileInput.setVisible(true);
-                } else {
+
+                    // hide the credit card details field
+                    oCCDetailsLabel.setVisible(false);
+                    oCCDetailsInput.setVisible(false);
+                }
+                else if (sSelectedKey === "CC"){
+                    // show the credit card details field
+                    oCCDetailsLabel.setVisible(true);
+                    oCCDetailsInput.setVisible(true);
+
+                    // hide the mobile field
                     oMobileLabel.setVisible(false);
                     oMobileInput.setVisible(false);
+                }
+                else {
+                    oMobileLabel.setVisible(false);
+                    oMobileInput.setVisible(false);
+                    oCCDetailsLabel.setVisible(false);
+                    oCCDetailsInput.setVisible(false);
                 }
             },
         
         onPressCheckout: function (){
                 var oInputFNameValue = this.getView().byId("idInptFName").getValue();
-
-                // Check if first name is blank
-                if (oInputFNameValue === ""){
+                var oInputLNameValue = this.getView().byId("idInptLName").getValue();
+                // Check if first name or last name is blank
+                if (oInputFNameValue === "" || oInputLNameValue === ""){
                     var oTextBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
                     var sErrMsg = oTextBundle.getText("checkoutButtonErrMsg");
                     sap.m.MessageToast.show(sErrMsg); 
